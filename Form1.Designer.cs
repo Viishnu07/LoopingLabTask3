916ce3678772fe9f2f8455ac6d2662fb48bc0059
viishnusreeganesh <viishnusreeganesh@VIISHNUSREE22A0>
2023-10-06 15:26:50 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace LoopingLabTask3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxCaloriesBurned = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBoxCaloriesBurned
+            // 
+            this.listBoxCaloriesBurned.FormattingEnabled = true;
+            this.listBoxCaloriesBurned.ItemHeight = 25;
+            this.listBoxCaloriesBurned.Location = new System.Drawing.Point(128, 210);
+            this.listBoxCaloriesBurned.Name = "listBoxCaloriesBurned";
+            this.listBoxCaloriesBurned.Size = new System.Drawing.Size(652, 304);
+            this.listBoxCaloriesBurned.TabIndex = 0;
+            this.listBoxCaloriesBurned.SelectedIndexChanged += new System.EventHandler(this.listBoxCaloriesBurned_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1201, 903);
+            this.Controls.Add(this.listBoxCaloriesBurned);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxCaloriesBurned;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoopingLabTask3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CalculateCaloriesBurned();
+        }
+
+        private void CalculateCaloriesBurned()
+        { 
+            listBoxCaloriesBurned.Items.Clear();
+            
+
+            double caloriesPerMinute = 3.9;
+
+            for (int minutes = 10; minutes <= 30; minutes += 5)
+            {
+                double caloriesBurned = caloriesPerMinute * minutes;
+                listBoxCaloriesBurned.Items.Add($"After {minutes} minutes: {caloriesBurned} calories");
+
+            }
+        }
+
+        private void listBoxCaloriesBurned_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
